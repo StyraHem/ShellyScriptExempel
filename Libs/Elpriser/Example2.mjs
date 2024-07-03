2d593d74a1e0d54eb754ae75df91b80a4e1fd91a
Håkan Åkerberg <hakan@tarra.se>
2024-07-03 15:22:16 +0200

    Update Example2.mjs

--- Libs/Elpriser/Example2.mjs
+++ Libs/Elpriser/Example2.mjs
@@ -1,1 +1,8 @@
-
+Shelly.emitEvent(
+  "startCheckElpris", 
+  {
+    region : "se3",  //Region se1, se2, se3, se4
+    token : "xxxxxxxxxxxxxxxxxxxxxxxx",  //Token from https://qtrl.me
+    interval: 600,  //Interval, min 60s
+  }
+);
