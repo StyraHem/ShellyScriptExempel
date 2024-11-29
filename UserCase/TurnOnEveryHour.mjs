bb58b06bf54c1403f29103ff628ae20f212ee9e5
Håkan Åkerberg <hakan@tarra.se>
2024-11-29 15:09:16 +0100

    Update TurnOnEveryHour.mjs

--- UserCase/TurnOnEveryHour.mjs
+++ UserCase/TurnOnEveryHour.mjs
@@ -1,18 +1,21 @@
-/* This script tur on a device every hour '/
+/* This script tur on a device every hour */
 
 function statusGet(id, callback){Shelly.call("Switch.GetStatus",{"id":id},function(r){callback(r.output)});}
 function statusSet(id, val){Shelly.call("Switch.Set",{"id":id, "on":val});}
-function getVal(){(new date()).getHours();}
+function getVal(){(new Date()).getHours();}
 
 let lastVal = getVal();
 
 function checkTime()
 {
+  Logger.log("Check time");
   var val = getVal();
   if (val != lastVal) {
+    Logger.log("Turn on");
     statusSet(0 , 1);
     lastVal = val;
   }
 }
 
+Logger.log("Starting");
 Timer.set(60000, true, checkTime);
