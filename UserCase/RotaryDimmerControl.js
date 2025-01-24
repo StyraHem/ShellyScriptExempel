39e3bc8f1d7b0ebfca2a74224d48b57a5901ac71
Håkan Åkerberg <hakan@tarra.se>
2025-01-24 14:48:55 +0100

    Create RotaryDimmerControl.js

---
+++ UserCase/RotaryDimmerControl.js
@@ -0,0 +1,23 @@
+/**
+ * This script is used to read the voltage from a rotary dimmer connected via a Shelly Plus Addon 
+ * and control the brightness of a dimmer connected to the Shelly device's output.
+ *
+ * Functionality:
+ * - The rotary dimmer's analog output is connected to the Addon's analog input.
+ * - When a voltage change is detected, the value is multiplied by 10 
+ *   (to match the dimmer's brightness scale of 0–100) and sent to the output dimmer.
+ *
+ * Requirements:
+ * - A Shelly Plus device with an Addon installed.
+ * - A rotary dimmer connected to the Addon's analog input.
+ * - A light/fan connected to the Shelly device's output (controlled via "Light.Set").
+ */
+
+Shelly.addEventHandler(function (event) {
+  const volt = event.info.voltage; // Read the voltage from the Addon's analog input
+  print(volt); // Debug print of the analog value
+  if (volt) {
+    // Set the brightness based on the analog value
+    Shelly.call("Light.Set", { id: 0, brightness: volt * 10 });
+  }
+});
