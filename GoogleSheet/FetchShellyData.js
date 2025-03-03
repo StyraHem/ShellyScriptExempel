dcd654e19cc97cb66260ba466da09c566a5453b4
Håkan Åkerberg <hakan@tarra.se>
2025-03-03 16:37:48 +0100

    Update FetchShellyData.js

--- GoogleSheet/FetchShellyData.js
+++ GoogleSheet/FetchShellyData.js
@@ -1,7 +1,14 @@
 /**
- * This is a script that can be used in Google Shhets to fetch consumption data from a Shelly 3EM and combined with 
+ * This is a script that can be used in Google Shhets to fetch consumption data from a Shelly  and combined with 
  * price information in sweden.
  *
+ * Autor: This scriped is created by StyraHem, Sweden
+ *
+ * Devices:
+ * - Shelly 3EM
+ * - Shelly Pro 3EM
+ * - Shelly Plus/G3 PM devices
+ 
  * Steps:
  * 1. Get the current hour
  * 3. Fetch and parse device status from Shelly API.
@@ -27,6 +34,23 @@ const QTRLME_TOKEN = 'XXXXXXX';
 //What price zone to ues; SE1, SE2, SE3, SE4 (only sweden)
 const PRICE_ZONE = 'SE3'
 
+//Find energy given bu aenergy object, for like Shelly PM devices
+function getEnergy(data) {
+  for (const key in data) {
+    if (typeof data[key] === 'object' && data[key] !== null) {
+      if (data[key].aenergy && data[key].aenergy.total) {
+        return data[key].aenergy.total;
+      } else {
+          const result = getEnergy(data[key]);
+          if(result){
+            return result;
+          }
+      }
+    }
+  }
+  return null; 
+}
+
 function fetchShellyData() {
   
   //Get date on hour level, by set min, sec to zero.
@@ -49,9 +73,12 @@ function fetchShellyData() {
   } else if ('emdata:0' in data.data.device_status) {
     //Pro 3EM
     totalSum = data.data.device_status['emdata:0'].total_act;
-  } else {
+  } else if ('pm1:0' in data.data.device_status) {
+    totalSum = 
+  }
+  else {
     Logger.log("Can't find total energy from server");
-    Logger.log(data.data.device_status['emdata:0'].total_act)
+    Logger.log(data.data.device_status)
   }
 
   const price = parseFloat(UrlFetchApp.fetch('https://qtrl.me/api/price/' + PRICE_ZONE + '?token=' + QTRLME_TOKEN))
