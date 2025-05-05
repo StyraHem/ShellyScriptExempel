42205d8c35cc2fb9e30b9690f3685f5bec2cf458
Håkan Åkerberg <hakan@tarra.se>
2025-05-05 10:42:31 +0200

    Update ChangeStateElectricalPrice.mjs
    Add an error counter so the error handler only starts after subsequent errors

--- UserCase/ChangeStateElectricalPrice.mjs
+++ UserCase/ChangeStateElectricalPrice.mjs
@@ -10,13 +10,16 @@ Developed by: StyraHem.se
 */
 const cfg = 
 {
-  region : "",  //Region se1, se2, se3, se4
-  token : "",  //Token from https://qtrl.me
-  interval: 60,  //Interval, min 60s
+  region : "",       //Region se1, se2, se3, se4
+  token : "",        //Token from https://qtrl.me
+  interval: 60,      //Interval, min 60s
+  switchId: 0,       //Change status of switch nr 0=first 1=2:nd
+  maxErrors: 5,      //When it get X errors in row the output will -
+  errorState: true,  //be changed to this value. Set maxErrors to 0 to 
+                     //turn off the error handler
 }
 
-const switchId = 0;   //Change status of switch nr 0=first 1=2:nd
-
+/* In this function you define when the output should urn on/off */
 function gotElpris(data)
 {  
   const state = (data.price < 0.5);      //On when price below 0.5
@@ -24,15 +27,21 @@ function gotElpris(data)
   //const state = (data.price < data.next_price)  //On when next price is more than current
 
   //Set state
-  Shelly.call("Switch.Set", {id:switchId, on:state});  
-}
-
-function error(type, msg) {
-  //Type: 1 - Cant connect to qtrl.me, 2 - Server error, 3 - Wrong response from server
-  Shelly.call("Switch.Set", {id:switchId, on:true});  //Turn on the output when error 
+  Shelly.call("Switch.Set", {id:cfg.switchId, on:state});  
 }
 
 /* DON'T CHANGE BELOW */
+let errorCnt = 0;
+
+function error(type, msg) {
+  if (cfg.maxErrors<=0)
+    return;
+  //Type: 1 - Cant connect to qtrl.me, 2 - Server error, 3 - Wrong response from server
+  if (errorCnt>=cfg.maxErrors)
+    Shelly.call("Switch.Set", {id:cfg.switchId, on:cfg.errorState});
+  else
+    errorCnt++;
+}
 
 function checkElpris() 
 {    
@@ -46,14 +55,15 @@ function checkElpris()
           print("ReqError:", error_message);
         } else if (result.code != 200) {
           error(2, error_message);
-          print("HttpError", result.message);      
+          print("HttpError:", result.message);      
         } else {
           const data = JSON.parse(result.body);
           print("Data=", data);
           if (data.price===undefined) {
             error(3, "Invalid JSON response, " + result.body);
-            print("DataError", "Invalid JSON response");      
-          } else {        
+            print("DataError:", "Invalid JSON response");      
+          } else {
+            errorCnt = 0;
             gotElpris(data);        
           }
         }
